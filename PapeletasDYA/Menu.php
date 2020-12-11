437634ec5dbc03f2fd74ae4b5255b956988e2d83
carlosesosa10 <carlosesosa10@gmail.com>
2020-12-11 11:29:08 -0600

    archivo php para Menu del proyecto

---
+++ PapeletasDYA/Menu.php
@@ -0,0 +1,27 @@
+
+<link rel="stylesheet" type="text/css" href="estilosmenu.css">
+<!-- Bootstrap core CSS -->
+<link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+<div align="left"> <img src="DYA.jpg"></div><p>
+<meta charset="utf-8">
+
+
+<h1>Papeletas en linea</h1>
+
+<nav>
+<a href="nuevo.php">Nueva</a>
+<a href="papeletas.php">Papeletas</a>
+<a href="lista.php">Lista</a>
+<a href="registros.php">registros</a>
+<a href="crear.php">Papeleta INDV</a>
+<!--<a href="cerrar-sesion.php">Cerrar Sesion</a>
+-->
+<div class="animation start-Nueva"></div>	
+</nav>
+
+<a href="cerrar-sesion.php" class="btn btn-success">Cerrar Sesion</a>
+
+
+
+
